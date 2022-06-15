e05140ae06cfb5b30e548935247a7231229a630c
grizB4 <105042165+grizB4@users.noreply.github.com>
2022-06-15 20:57:21 +0100

    Add files via upload

---
+++ Calculator/main.js
@@ -0,0 +1,14 @@
+//Global Variables
+let equals_btn = document.getElementById('equals');
+let calculator_total = document.getElementById('ash');
+console.log(equals_btn)
+
+equals_btn.addEventListener('click', equals);
+
+function equals () {
+console.log("function running")
+};
+
+console.log(calculator_total.innerText);
+
+calculator_total.innerText = "cunt";
